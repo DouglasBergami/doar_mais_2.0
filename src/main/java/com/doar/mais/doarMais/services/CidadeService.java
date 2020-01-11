fee3f6d8d2b006227756dd5b2ae7c52abafd9fe6
DouglasBergami <douglas.t.k.d@hotmail.com>
2020-01-11 16:21:20 -0200

    criando service de Cidade

--- src/main/java/com/doar/mais/doarMais/services/CidadeService.java
+++ src/main/java/com/doar/mais/doarMais/services/CidadeService.java
@@ -4,8 +4,11 @@ import com.doar.mais.doarMais.domains.Cidade;
 import com.doar.mais.doarMais.repositories.CidadeRepository;
 import com.doar.mais.doarMais.services.Exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,6 +23,31 @@ public class CidadeService {
 
         return obj.orElseThrow(
                 () -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cidade.class));
+    }
+
+    public List<Cidade> findAll(){
+        return cidadeRepository.findAll();
+    }
+
+    @Transactional
+    public Cidade insert(Cidade obj){
+        obj.setId(null);
+        return cidadeRepository.save(obj);
+    }
+
+    public void delete(Long id){
+        findById(id);
+
+        try{
+            cidadeRepository.deleteById(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DataIntegrityViolationException("");
+        }
+
+    }
+
+    public Cidade update(Cidade obj){
+        return cidadeRepository.save(obj);
     }
 
 }
